c524711f697162bb867f16eb5080131b0751e545
ttoha <toooxa@gmail.com>
2017-12-10 23:04:57 +0200

    new design

---
+++ js/functions.js
@@ -0,0 +1,104 @@
+jQuery(document).ready(function ($) {
+    $('[data-slider="main-slider"]').each(function(){
+      var el = $(this);
+
+      el.find('[data-slider-slides]').slick({
+          adaptiveHeight: false,
+          slidesToShow: 1,
+          prevArrow: el.find('[data-slider-arrow-left]'),
+          nextArrow: el.find('[data-slider-arrow-right]'),
+      });
+    });
+    $('[data-slider="second-slider"]').each(function(){
+      var el = $(this);
+
+      el.find('[data-slider-slides]').slick({
+          adaptiveHeight: false,
+          slidesToShow: 4,
+          dots: true,
+          arrows: false
+      });
+    });
+    $('[data-slider="testimonials-slider"]').each(function(){
+        var el = $(this);
+
+        el.find('[data-slider-slides]').slick({
+            adaptiveHeight: false,
+            slidesToShow: 2,
+            dots: true,
+            arrows: false
+        });
+    });
+
+    $('video').mediaelementplayer({
+        alwaysShowControls: false,
+        videoVolume: 'horizontal',
+        features: ['playpause','progress','volume','fullscreen']
+    });
+});
+
+
+$( function() {
+
+    ( function( factory ) {
+        if ( typeof define === "function" && define.amd ) {
+
+            // AMD. Register as an anonymous module.
+            define( [ "../widgets/datepicker" ], factory );
+        } else {
+
+            // Browser globals
+            factory( jQuery.datepicker );
+        }
+    }( function( datepicker ) {
+
+        datepicker.regional.ru = {
+            closeText: "Закрыть",
+            prevText: "&#x3C;Пред",
+            nextText: "След&#x3E;",
+            currentText: "Сегодня",
+            monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
+                "Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+            monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+                "Июл","Авг","Сен","Окт","Ноя","Дек" ],
+            dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+            dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+            dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+            weekHeader: "Нед",
+            dateFormat: "dd.mm.yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "" };
+        datepicker.setDefaults( datepicker.regional.ru );
+
+        return datepicker.regional.ru;
+
+    } ) );
+
+
+
+    $('#datepicker').datepicker($.extend({
+            inline: true
+        }
+    ));
+
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+} );
+
+
+var map;
+function initMap() {
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 50.451287, lng: 30.525360},
+        zoom: 14,
+        disableDefaultUI: true
+    });
+
+    var marker = new google.maps.Marker({
+        position: {lat: 50.451287, lng: 30.525360},
+        map: map,
+        icon: 'img/pin.png'
+    });
+};
