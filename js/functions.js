a3fa9b012664e81390556cd633229596d24e7611
ttoha <toooxa@gmail.com>
2018-03-14 01:05:12 +0200

    fix small bugs

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,51 @@
+$( function() {
+
+    ( function( factory ) {
+        if ( typeof define === "function" && define.amd ) {
+
+            // AMD. Register as an anonymous module.
+            define( [ "../widgets/datepicker" ], factory );
+        } else {
+
+            // Browser globals
+            factory( jQuery.datepicker );
+        }
+    }( function( datepicker ) {
+
+        datepicker.regional.ru = {
+            closeText: "Закрыть",
+            prevText: "&#x3C;Пред",
+            nextText: "След&#x3E;",
+            currentText: "Сегодня",
+            monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
+                "Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+            monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
+                "Июл","Авг","Сен","Окт","Ноя","Дек" ],
+            dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
+            dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
+            dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+            weekHeader: "Нед",
+            dateFormat: "dd.mm.yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "" };
+        datepicker.setDefaults( datepicker.regional.ru );
+
+        return datepicker.regional.ru;
+
+    } ) );
+
+    $('#datepicker').datepicker($.extend({
+            inline: true
+        }
+    ));
+
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+
+    $('[data-datepicker]').datepicker()
+} );
+
 jQuery(document).ready(function ($) {
 
     $('[data-modal]').magnificPopup({
@@ -104,52 +152,6 @@ jQuery(document).ready(function ($) {
         },
     });
 });
-
-$( function() {
-
-    ( function( factory ) {
-        if ( typeof define === "function" && define.amd ) {
-
-            // AMD. Register as an anonymous module.
-            define( [ "../widgets/datepicker" ], factory );
-        } else {
-
-            // Browser globals
-            factory( jQuery.datepicker );
-        }
-    }( function( datepicker ) {
-
-        datepicker.regional.ru = {
-            closeText: "Закрыть",
-            prevText: "&#x3C;Пред",
-            nextText: "След&#x3E;",
-            currentText: "Сегодня",
-            monthNames: [ "Январь","Февраль","Март","Апрель","Май","Июнь",
-                "Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
-            monthNamesShort: [ "Янв","Фев","Мар","Апр","Май","Июн",
-                "Июл","Авг","Сен","Окт","Ноя","Дек" ],
-            dayNames: [ "воскресенье","понедельник","вторник","среда","четверг","пятница","суббота" ],
-            dayNamesShort: [ "вск","пнд","втр","срд","чтв","птн","сбт" ],
-            dayNamesMin: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
-            weekHeader: "Нед",
-            dateFormat: "dd.mm.yy",
-            firstDay: 1,
-            isRTL: false,
-            showMonthAfterYear: false,
-            yearSuffix: "" };
-        datepicker.setDefaults( datepicker.regional.ru );
-
-        return datepicker.regional.ru;
-
-    } ) );
-
-    $('#datepicker').datepicker($.extend({
-            inline: true
-        }
-    ));
-
-    $.datepicker.setDefaults($.datepicker.regional['ru']);
-} );
 
 var map;
 function initMap() {
